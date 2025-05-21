b08fc8d62c4710d91f2e38aca6e839cb3c2c50e4
un2wo <45891360+un2wo@users.noreply.github.com>
2025-05-21 16:26:12 -0500

    real-ish palette refinements

--- shaders/lib/color_utils.glsl
+++ shaders/lib/color_utils.glsl
@@ -169,18 +169,18 @@ uniform int moonPhase;
 #elif COLOR_SCHEME == 10  // Real-ish
     #define OMNI_TINT 0.25
     #define LIGHT_SUNSET_COLOR vec3(1.0, 0.59, 0.35)
-    #define LIGHT_DAY_COLOR vec3(1.05, 1.05, 1)
+    #define LIGHT_DAY_COLOR vec3(1.1, 1.05, 0.95)
     #define LIGHT_NIGHT_COLOR vec3(0.015, 0.02, 0.03) * NIGHT_BRIGHT_PHASE
 
     #define ZENITH_SUNSET_COLOR vec3(0.25, 0.25, 0.36)
-    #define ZENITH_DAY_COLOR vec3(0.4, 0.6, 0.85)
+    #define ZENITH_DAY_COLOR vec3(0.4, 0.55, 0.85)
     #define ZENITH_NIGHT_COLOR vec3(0.004, 0.006, 0.009) * NIGHT_BRIGHT_PHASE
 
     #define HORIZON_SUNSET_COLOR vec3(0.79, 0.6, 0.5)
     #define HORIZON_DAY_COLOR vec3(0.65, 0.85, 1.5)
     #define HORIZON_NIGHT_COLOR vec3(0.011, 0.015, 0.02) * NIGHT_BRIGHT_PHASE
 
-    #define WATER_COLOR vec3(0.05, 0.1, 0.11)
+    #define WATER_COLOR vec3(0.05, 0.1, 0.1)
 #elif COLOR_SCHEME == 99 // Custom
     #define OMNI_TINT OMNI_TINT_CUSTOM
     #define LIGHT_SUNSET_COLOR vec3(LIGHT_SUNSET_COLOR_R, LIGHT_SUNSET_COLOR_G, LIGHT_SUNSET_COLOR_B)
