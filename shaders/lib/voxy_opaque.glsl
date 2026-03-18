5d40bcaf9fd74dc1bfeda0c32767965fb0169db3
un2wo <45891360+un2wo@users.noreply.github.com>
2026-03-18 00:00:22 -0500

    material gloss in lod chunks

--- shaders/lib/voxy_opaque.glsl
+++ shaders/lib/voxy_opaque.glsl
@@ -27,6 +27,10 @@
 //    uint customId;//Same as iris's modelId
 // };
 
+#if defined MATERIAL_GLOSS && !defined NETHER
+    #include "/lib/material_gloss_fragment.glsl"
+#endif
+
 layout(location = 0) out vec4 block_color;
 
 void voxy_emitFragment(VoxyFragmentParameters param) {
@@ -38,10 +42,48 @@ void voxy_emitFragment(VoxyFragmentParameters param) {
 	vec3 final_candle_color = candle_color;
 	float shadow_c = abs((light_mix * 2.0) - 1.0);    
 
-	vec3 real_light =
-		omni_light +
-		(shadow_c * direct_light_color * direct_light_strength) * (1.0 - (rainStrength * 0.75)) +
-		final_candle_color;
+    #if defined MATERIAL_GLOSS && !defined NETHER
+		vec4 sub_position = vxModelView * worldPos;
+		vec3 sub_position3 = sub_position.xyz;
+		
+		float gloss_factor = 1.05;
+		float gloss_power = 6.0;
+		float luma_factor = 1.0;
+		float luma_power = 2.0;
+
+        if (sand == 1) {  // Sand-like block
+            luma_power = 4.0;
+        } else if (metal == 1) {  // Metal-like block
+            luma_factor = 1.35;
+            luma_power = -1.0;
+            gloss_power = 100.0;
+        } else if (fabric == 1) {  // Fabric-like blocks
+            gloss_power = 3.0;
+            gloss_factor = 0.1;
+        }
+
+        float final_gloss_power = gloss_power;
+        block_luma *= luma_factor;
+
+        if(luma_power < 0.0) {  // Metallic
+            final_gloss_power -= (block_luma * 73.334);
+        } else {
+            block_luma = pow(block_luma, luma_power);
+        }
+
+        float material_gloss_factor = material_gloss(reflect(normalize(sub_position3), normal), param.lightMap, final_gloss_power, normal) * gloss_factor;
+
+        float material = material_gloss_factor * block_luma;
+        vec3 real_light =
+			omni_light +
+			(shadow_c * ((direct_light_color * direct_light_strength) + (direct_light_color * material))) * (1.0 - (rainStrength * 0.75)) +
+			final_candle_color;
+    #else
+		vec3 real_light =
+			omni_light +
+			(shadow_c * direct_light_color * direct_light_strength) * (1.0 - (rainStrength * 0.75)) +
+			final_candle_color;
+	#endif
 
 	block_color.rgb *= mix(real_light, vec3(1.0), nightVision * 0.125);
 	block_color.rgb *= mix(vec3(1.0, 1.0, 1.0), vec3(NV_COLOR_R, NV_COLOR_G, NV_COLOR_B), nightVision);
