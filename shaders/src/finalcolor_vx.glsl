4ccb6559392365e3c9f0860131185132b66f4a32
un2wo <45891360+un2wo@users.noreply.github.com>
2026-03-18 03:04:24 -0500

    fix fog toggle

--- shaders/src/finalcolor_vx.glsl
+++ shaders/src/finalcolor_vx.glsl
@@ -5,8 +5,10 @@
         block_color.rgb = mix(block_color.rgb, mix(fogColor * 0.1, vec3(1.0), 0.04), frog_adjust);
     #endif
 #else
-    vec3 fog_texture = texture2D(gaux4, gl_FragCoord.xy * vec2(pixel_size_x, pixel_size_y)).rgb;
-    block_color.rgb = mix(block_color.rgb, fog_texture, frog_adjust);
+    #ifdef FOG_ACTIVE
+        vec3 fog_texture = texture2D(gaux4, gl_FragCoord.xy * vec2(pixel_size_x, pixel_size_y)).rgb;
+        block_color.rgb = mix(block_color.rgb, fog_texture, frog_adjust);
+    #endif
 #endif
 
 #if MC_VERSION >= 11900
