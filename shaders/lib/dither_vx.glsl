4a94b4cfe75fe59982a92c90c50d762cd02f022a
un2wo <45891360+un2wo@users.noreply.github.com>
2026-03-20 20:10:16 -0500

    Delete shaders/lib/dither_vx.glsl
    unused now

--- shaders/lib/dither_vx.glsl
+++
@@ -1,7 +0,0 @@
-float r_dither(vec2 frag) {
-    return fract(dot(frag, vec2(0.75487766624669276, 0.569840290998)));
-}
-
-float shifted_r_dither(vec2 frag) {
-    return fract(dither_shift + dot(frag, vec2(0.75487766624669276, 0.569840290998)));
-}
