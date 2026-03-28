5bab08b2eb1affffe3af5324a88e18e943f1e4f4
un2wo <45891360+un2wo@users.noreply.github.com>
2026-03-28 13:38:20 -0500

    buffer thing
    fixes nether ice reflections being black when above the roof

--- shaders/src/writebuffers.glsl
+++ shaders/src/writebuffers.glsl
@@ -1,25 +1,43 @@
-#ifdef WATER_F
-    /* DRAWBUFFERS:1 */
-    gl_FragData[0] = block_color;
-#elif (defined SPECIAL_TRANS && MC_VERSION >= 11300) || defined GBUFFER_HAND_WATER
-    /* DRAWBUFFERS:1 */
-    gl_FragData[0] = block_color;
-#else
-    #if defined SET_FOG_COLOR
-        /* DRAWBUFFERS:17 */
-        gl_FragData[0] = vec4(block_color, 1.0);
-        gl_FragData[1] = vec4(block_color, 1.0);
-    #elif MC_VERSION < 11604 && defined GBUFFER_SKYBASIC
-        /* DRAWBUFFERS:17 */
-        gl_FragData[0] = block_color;
-        gl_FragData[1] = block_color;
-    #else
-        #if defined VOXY
+#ifdef VOXY
+	#ifdef WATER_F
+		/* DRAWBUFFERS:18 */
+		gl_FragData[0] = block_color;
+		gl_FragData[1] = block_color;
+	#elif (defined SPECIAL_TRANS && MC_VERSION >= 11300) || defined GBUFFER_HAND_WATER
+		/* DRAWBUFFERS:18 */
+		gl_FragData[0] = block_color;
+		gl_FragData[1] = block_color;
+	#else
+		#if defined SET_FOG_COLOR
+		    /* DRAWBUFFERS:178 */
+		    gl_FragData[0] = vec4(block_color, 1.0);
+		    gl_FragData[1] = vec4(block_color, 1.0);
+		    gl_FragData[2] = vec4(block_color, 1.0);
+		#else
 		    /* DRAWBUFFERS:18 */
-		    gl_FragData[0] = block_color;
-        #else
-            /* DRAWBUFFERS:1 */
 			gl_FragData[0] = block_color;
-        #endif
-    #endif
+			gl_FragData[1] = block_color;
+		#endif
+	#endif
+#else
+	#ifdef WATER_F
+		/* DRAWBUFFERS:1 */
+		gl_FragData[0] = block_color;
+	#elif (defined SPECIAL_TRANS && MC_VERSION >= 11300) || defined GBUFFER_HAND_WATER
+		/* DRAWBUFFERS:1 */
+		gl_FragData[0] = block_color;
+	#else
+		#if defined SET_FOG_COLOR
+		    /* DRAWBUFFERS:17 */
+		    gl_FragData[0] = vec4(block_color, 1.0);
+		    gl_FragData[1] = vec4(block_color, 1.0);
+		#elif MC_VERSION < 11604 && defined GBUFFER_SKYBASIC
+		    /* DRAWBUFFERS:17 */
+		    gl_FragData[0] = block_color;
+		    gl_FragData[1] = block_color;
+		#else
+		    /* DRAWBUFFERS:1 */
+			gl_FragData[0] = block_color;
+		#endif
+	#endif
 #endif
