143a68758458ff4b20f07004a3400503249a7749
un2wo <45891360+un2wo@users.noreply.github.com>
2026-07-14 15:44:28 -0500

    fog color
    fog during sunset now takes less influence from hi_sky

--- shaders/src/finalcolor_vx.glsl
+++ shaders/src/finalcolor_vx.glsl
@@ -9,7 +9,7 @@
 		vec3 fog_texture = texture2D(gaux4, gl_FragCoord.xy * vec2(pixel_size_x, pixel_size_y)).rgb;
 		block_color.rgb = mix(
 			block_color.rgb,
-			mix(mix(fog_texture, hi_sky_color_rgb, frog_adjust * 0.5 + 0.25), fog_texture, frog_adjust2),
+			mix(mix(fog_texture, hi_sky_color_rgb, frog_adjust * day_blend_float(0.0, 0.75, 0.75) + 0.25), fog_texture, frog_adjust2),
 			frog_adjust
 		);
     #endif
