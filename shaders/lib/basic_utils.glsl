f0e13edddce4136a1731d48c6dd9611f372c9441
un2wo <45891360+un2wo@users.noreply.github.com>
2026-08-18 15:30:02 -0500

    options + rearranging

--- shaders/lib/basic_utils.glsl
+++ shaders/lib/basic_utils.glsl
@@ -71,3 +71,24 @@ vec4 vec3_sixth_pow(vec4 x) {
     vec4 temp_2 = x * x;
     return temp_2 * temp_2 * temp_2;
 }
+
+// sorry for beinsg a stupid chud vibecoder (adjustable smoothstep based on Peter Stock's function)
+// https://www.peterstock.co.uk/games/adjustable_smoothstep/
+vec3 adjustable_smoothstep(vec3 x, float p, float g) {
+    float c = (g - 1.0) / (2.0 - g); // g can't be 2.0, so just do 1.999 or smth
+    vec3 y;
+    for (int i = 0; i < 3; ++i) {
+        float xi = clamp(x[i], 0.0, 1.0);
+        if (xi < p) {
+            float num = xi * xi * (1.0 + c);
+            float den = xi + p * c;
+            y[i] = num / den;
+        } else {
+            float xm = 1.0 - xi;
+            float num = xm * xm * (1.0 + c);
+            float den = xm + (1.0 - p) * c;
+            y[i] = 1.0 - (num / den);
+        }
+    }
+    return y;
+}
